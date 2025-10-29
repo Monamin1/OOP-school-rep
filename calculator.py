44ab3a011473595ff37efbbf8069d504357942df
AlvinJohnDano <qajsdano@tip.edu.ph>
2025-10-29 16:11:02 +0800

    new file:   calculator.py 	new file:   duenas.txt 	new file:   gui_grid1.py 	new file:   gui_grid2.py 	new file:   gui_simplenotepad.py

---
+++ calculator.py
@@ -0,0 +1,5 @@
+# Make a calculator program that can compute perform the Arithmetic operations as well as exponential operation, sin, cosine
+# math functions as well clearing using the C button and/or clear from a menu bar. The calculator must be able to store and
+# retrieve the operations and results in a text file. A file menu should be available and have the option Exit which should also be
+# triggered when ctrl+Q is pressed on the keyboard. You may refer to your calculator program in the Desktop.
+
